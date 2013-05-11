de57d57bfb01c3013c8808f8c87e5e9a64058759
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-05-11 10:47:03 -0400

    updating our urls

--- elvis/elvis/urls.py
+++ elvis/elvis/urls.py
@@ -6,6 +6,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from elvis.views.main import home
 from elvis.views.piece import PieceList, PieceDetail
 from elvis.views.corpus import CorpusList, CorpusDetail
+from elvis.views.user import UserList, UserDetail
+from elvis.views.movement import MovementList, MovementDetail
+from elvis.views.composer import ComposerList, ComposerDetail
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,14 +19,16 @@ urlpatterns = []
 urlpatterns += format_suffix_patterns(
     patterns('',
         url(r'^$', home, name='home'),
+        url(r'^users/$', UserList.as_view(), name="user-list"),
+        url(r'^user/(?P<pk>[0-9]+)/$', UserDetail.as_view(), name="user-detail"),
         url(r'^pieces/$', PieceList.as_view(), name="piece-list"),
         url(r'^piece/(?P<pk>[0-9]+)/$', PieceDetail.as_view(), name="piece-detail"),
         url(r'^corpora/$', CorpusList.as_view(), name="corpus-list"),
         url(r'^corpus/(?P<pk>[0-9]+)/$', CorpusDetail.as_view(), name="corpus-detail"),
-        url(r'^composers/$', PieceList.as_view(), name="piece-list"),
-        url(r'^composer/(?P<pk>[0-9]+)/$', PieceDetail.as_view(), name="piece-detail"),
-        url(r'^movements/$', PieceList.as_view(), name="piece-list"),
-        url(r'^movement/(?P<pk>[0-9]+)/$', PieceDetail.as_view(), name="piece-detail"),
+        url(r'^composers/$', ComposerList.as_view(), name="composer-list"),
+        url(r'^composer/(?P<pk>[0-9]+)/$', ComposerDetail.as_view(), name="composer-detail"),
+        url(r'^movements/$', MovementList.as_view(), name="movement-list"),
+        url(r'^movement/(?P<pk>[0-9]+)/$', MovementDetail.as_view(), name="movement-detail"),
     )
 )
 
