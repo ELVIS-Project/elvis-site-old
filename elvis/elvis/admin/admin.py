3af1647ab66dcfadac660d54808aae8da5882e41
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-05-11 11:52:49 -0400

    update admin.py with tag admin

--- elvis/elvis/admin/admin.py
+++ elvis/elvis/admin/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from elvis.models.piece import Piece
 from elvis.models.corpus import Corpus
 from elvis.models.composer import Composer
+from elvis.models.tag import Tag
+from elvis.models.tag_hierarchy import TagHierarchy
 
 
 class PieceAdmin(admin.ModelAdmin):
@@ -16,6 +18,17 @@ class CorpusAdmin(admin.ModelAdmin):
 class ComposerAdmin(admin.ModelAdmin):
     pass
 
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "old_id")
+
+
+class TagHierarchyAdmin(admin.ModelAdmin):
+    list_display = ("tag", "parent")
+
+
+admin.site.register(TagHierarchy, TagHierarchyAdmin)
 admin.site.register(Piece, PieceAdmin)
+admin.site.register(Tag, TagAdmin)
 admin.site.register(Corpus, CorpusAdmin)
 admin.site.register(Composer, ComposerAdmin)
