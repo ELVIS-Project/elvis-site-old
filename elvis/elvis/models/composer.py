b723c363c33fdc685b20968a2975ec3c5ecbfdb5
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-05-11 12:11:45 -0400

    make birth/death date nullable

--- elvis/elvis/models/composer.py
+++ elvis/elvis/models/composer.py
@@ -4,8 +4,11 @@ from django.db import models
 class Composer(models.Model):
     old_id = models.IntegerField()
     name = models.CharField(max_length=255)
-    birth_date = models.DateField()
-    death_date = models.DateField()
+    birth_date = models.DateField(blank=True, null=True)
+    death_date = models.DateField(blank=True, null=True)
+
+    def __unicode__(self):
+        return u"{0}".format(self.name)
 
     class Meta:
         app_label = "elvis"
