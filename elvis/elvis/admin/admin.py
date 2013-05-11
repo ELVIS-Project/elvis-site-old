a15b536538b3dda665318561274f6018c8773445
Morgane Ciot <morganeciot@wpa068015.wireless.mcgill.ca>
2013-05-11 14:02:06 -0400

    first commit

--- elvis/elvis/admin/admin.py
+++ elvis/elvis/admin/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from elvis.models.piece import Piece
 from elvis.models.corpus import Corpus
 from elvis.models.composer import Composer
-
+from elvis.models.movement import Movement
 
 class PieceAdmin(admin.ModelAdmin):
     pass
@@ -13,9 +13,24 @@ class CorpusAdmin(admin.ModelAdmin):
     pass
 
 
+# Can display CSS here
+
 class ComposerAdmin(admin.ModelAdmin):
-    pass
+	def upper_case_name(obj): return ("%s" % (obj.name)).upper()
+	
+	upper_case_name.short_description = 'name'
+
+	list_display = ("name", "birth_date", "death_date")
+
+	#list_display = (upper_case_name,)
+
+
+class MovementAdmin(admin.ModelAdmin):
+	pass
+
+# Indicates that these objects have admin interface 
 
 admin.site.register(Piece, PieceAdmin)
 admin.site.register(Corpus, CorpusAdmin)
 admin.site.register(Composer, ComposerAdmin)
+admin.site.register(Movement, MovementAdmin)
