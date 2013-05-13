33f777f4acdb14faf2d06f775727dc0a2ffb13c0
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-05-13 11:37:48 -0400

    update corpus model

--- elvis/elvis/models/corpus.py
+++ elvis/elvis/models/corpus.py
@@ -3,10 +3,13 @@ from django.contrib.auth.models import User
 
 
 class Corpus(models.Model):
-    old_id = models.IntegerField()
+    old_id = models.IntegerField(db_index=True)
     creator = models.ForeignKey(User)
     title = models.CharField(max_length=255, blank=True, null=True)
     comment = models.TextField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return u"{0}".format(self.title)
