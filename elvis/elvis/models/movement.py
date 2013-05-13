b373d6305533f489679c0df159b59afcbd7b19a3
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-05-12 23:08:03 -0400

    updated movement model

--- elvis/elvis/models/movement.py
+++ elvis/elvis/models/movement.py
@@ -5,14 +5,20 @@ from django.contrib.auth.models import User
 class Movement(models.Model):
     old_id = models.IntegerField()
     title = models.CharField(max_length=255)
-    uploader = models.ForeignKey(User)
-    corpus = models.ForeignKey("elvis.Corpus")
-    composer = models.ForeignKey("elvis.Composer")
+    uploader = models.ForeignKey(User, blank=True, null=True)
+    piece = models.ForeignKey("elvis.Piece", blank=True, null=True)
+    composer = models.ForeignKey("elvis.Composer", blank=True, null=True)
     date_of_composition = models.DateField(blank=True, null=True)
     number_of_voices = models.IntegerField(blank=True, null=True)
     tags = models.ManyToManyField("elvis.Tag", blank=True, null=True)
     attachments = models.ManyToManyField("elvis.Attachment", blank=True, null=True)
     comment = models.TextField(blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return u"{0}".format(self.title)
 
     class Meta:
         app_label = "elvis"
